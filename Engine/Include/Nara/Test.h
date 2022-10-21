841220facd369a860c4d1f1364920447c89a1370
Eearslya Sleiarion <eearslya@gmail.com>
2022-10-21 11:59:41 -0700

    Remove test files

--- Engine/Include/Nara/Test.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <Nara/Defines.h>
-
-NAPI void Nara_Print(I32 i);
--- Engine/Source/Test.c
+++
@@ -1,6 +0,0 @@
-#include <Nara/Test.h>
-#include <stdio.h>
-
-void Nara_Print(I32 i) {
-	printf("[Nara] %d\n", i);
-}
